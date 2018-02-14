c790b670e4e3bc2ad7ae22054db9da3bab71c52a
Martin Tellevik <146157@stud.hvl.no>
2018-02-14 10:08:32 +0100

    Room update

--- Model/Room.cs
+++ Model/Room.cs
@@ -4,5 +4,16 @@ namespace Model
 {
     public class Room
     {
+        public Room(int nr, int type, bool vasket, bool opptatt)
+        {
+            this.nr = nr;
+            this.type = type;
+            this.vasket = vasket;
+            this.opptatt = opptatt;
+        }
+        public int nr { get; set; }
+        public int type { get; set; }
+        public bool vasket { get; set; }
+        public bool opptatt { get; set; }
     }
 }
