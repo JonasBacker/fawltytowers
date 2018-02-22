4e3b16c504542364ad62c5ce0533bad6284fe1cf
fhqwhgads <bendik_b2@hotmail.com>
2018-02-22 15:27:47 +0100

    Revert "removed myDataTemplateSelector, as it was no longer being used"
    This reverts commit 84d99e1d56f6bfb858eb21c272bb2d163c4f4383.

---
+++ StaffUtility/MyDataTemplateSelector.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace StaffUtility
+{
+   public sealed class MyDataTemplateSelector : DataTemplateSelector
+    {
+        public DataTemplate DefaultStateTemplate { get; set; }
+        public DataTemplate SelectedStateItemTemplate { get; set; }
+
+        protected override DataTemplate SelectTemplateCore(object item,
+                                                           DependencyObject container)
+        {
+                return DefaultStateTemplate;
+        }
+    }
+}
